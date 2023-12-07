e8dff0af93f2c3b838113e88723da9c158cc1a0f
Anuskota <anamangada.dev@gmail.com>
2023-12-07 19:44:04 +0100

    change the text

--- public/js/text.js
+++ public/js/text.js
@@ -2,13 +2,9 @@ let btn = document.getElementById('play-btn');
 
 btn.addEventListener('click', function () {
     console.log("Se hizo click en el botton");
-
-})
-
-
-function changeText() {
     var text = document.getElementById('txt');
-    text.innerHTML = "He cambiado de titulo";
+    text.innerHTML = "Stay focused on the ball.";
 
 
-}
+});
+
