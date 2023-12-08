1c447f3e1f206aed652f54e5d3cc8c9bc3446bf4
Tania-Serrulla <taniaserrulla.95@gmail.com>
2023-12-08 17:35:09 +0100

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,45 @@
-var contador = 30;
+/*
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>titulo</title>
 
-function escribe(){
-    contador--,
-    document.getElementById('parrafo').innerHTML = contador;
-}
+  <style>
+    p{
+      font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+      font-size: 250px;
+      color: rgb(31, 217, 241);
+      display: flex;
+      justify-content: center;
+    }
+  </style>
 
-var intervalo;
+</head>
+<body>
+  <button id="btn-iniciar">INICIAR</button>
+  <button id="btn-detener">DETENER</button>
 
-document.getElementById('btn-iniciar').addEventListener('click',function(){intervalo = setInterval(escribe, 1000)});
+  <p id='parrafo'></p>
+  <script>
+    var contador = 30;
 
-document.getElementById('btn-detener').addEventListener('click',function(){clearInterval(intervalo)});
+    function escribe(){
+      contador--,
+      document.getElementById('parrafo').innerHTML = contador;
+    }
+
+    var intervalo;
+
+    document.getElementById('btn-iniciar').addEventListener('click',function(){intervalo = setInterval(escribe, 1000)});
+
+    document.getElementById('btn-detener').addEventListener('click',function(){clearInterval(intervalo)});
+
+  </script>
+
+  
+</body>
+</html>
+*/
