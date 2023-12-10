04c15a4b629a36eaedcbd03f6c1434dad0a27ff0
yamiranea <yamilarenea@gmail.com>
2023-12-10 17:34:43 +0100

    import and export added to js documents

--- public/js/main.js
+++ public/js/main.js
@@ -13,3 +13,10 @@ function submitForm(event) {
 
 //funciones para vincular el contador de rondas
 import { incrementRound, resetRoundDisplay } from "./scoreboard-rounds.js";
+
+//funciones para vincular los puntos ganadores o perdedores
+import {
+  incrementWinnerPoints,
+  incrementLoserPoints,
+  resetPointsDisplay,
+} from "./scoreboard-points.js";
